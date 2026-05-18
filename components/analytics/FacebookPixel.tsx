368d24ecbe45d77c0bc05a373194af014407ec0f
zyaddhamedd <zyaddhamedd@gmail.com>
2026-05-19 00:03:06 +0300

    fix(pixel): disable autoConfig automatic button tracking to prevent invalid currency errors

--- components/analytics/FacebookPixel.tsx
+++ components/analytics/FacebookPixel.tsx
@@ -53,6 +53,7 @@ export default function FacebookPixel() {
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('set', 'autoConfig', false, '${FB_PIXEL_ID}');
             fbq('init', '${FB_PIXEL_ID}');
             fbq('track', 'PageView');
           `,
